131c8ce9f596624b0deb55dfb4d313f30e7476a4
Alex Ferreira <alex.ferreira1796@gmail.com>
2024-05-18 18:28:04 -0300

    chore: remove typeorm informations

--- typeOrm.config.ts
+++ typeOrm.config.ts
@@ -6,11 +6,11 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST || "localhost",
-  port: parseInt(process.env.DB_PORT || "5432") || 5432,
-  username: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "mac@postgres",
-  database: process.env.DB_NAME || "codes",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: true,
   logging: ["query", "error"],
   entities: ["./src/db/entities/**/*.ts"],
